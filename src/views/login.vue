ade3eab427a25a7c494b64191143b488af1b0dca
sy2729 <sy2729@tc.columbia.edu>
2018-09-20 00:05:39 -0400

    initialized and made login pagr

---
+++ src/views/login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="center-box flex flex-justify-center login">
+      <div class="cover">
+        <img :src="url" alt="img">
+      </div>
+      <div class='content flex-auto'>
+        <h1 class="t-center">Log In</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      url:'https://cdn.dribbble.com/users/78489/screenshots/4966994/ramin_nasibov_logo__svrf_branding_logofolio_.png',
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+  
+  .login {
+    min-width: 350px;
+    width: 60%;
+    max-width: 700px;
+    height: 400px;
+  }
+
+  .cover {
+    width: 50%;
+  }
+
+  @media screen and (max-width: 800px){
+    .cover {display: none;}
+  }
+
+</style>
+
